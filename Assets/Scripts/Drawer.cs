8920f1c383af16d81cc647d07b98fced8d1492e7
Ready Nurse One <vrnurseone@gmail.com>
2020-02-19 16:29:31 -0600

    draw with interactable pill bottle

---
+++ Assets/Scripts/Drawer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : MonoBehaviour
+{
+
+    Vector3 initialPosition;
+    public Vector3 maxPositiveDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = gameObject.transform.localPosition;
+    }
+
+
+    void FixedUpdate()
+    {
+        Debug.Log(transform.localPosition);
+        if( transform.localPosition.z > initialPosition.z + maxPositiveDistance.z)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, initialPosition.z + maxPositiveDistance.z);
+        }
+    }
+}
